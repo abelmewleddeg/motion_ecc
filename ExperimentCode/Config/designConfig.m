1493a5a2c28dc3d7d4bae1fd40865b5d0049b3c4
raniaezzo <ezzo.rania@gmail.com>
2023-12-08 16:56:37 +0400

    fixed locations to alternative odd vs even runs and odd even subjects. check this

--- ExperimentCode/Config/designConfig.m
+++ ExperimentCode/Config/designConfig.m
@@ -15,40 +15,19 @@ function [expDes]=designConfig(scr,const)
 
 disp('~~~~~ DESIGN ~~~~~~')
 
- possiblePAs = [[45,135];[225,315]]
+ possiblePAs = [[45,135];[225,315]];
 
-if const.block==1 || const.DEBUG
-    %TO DO: pick ;2 random locations from possiblePAs;
-    const.PArandi = randperm(length(possiblePAs(1,:)))
+ if mod(const.subjID_numeric,2) % if odd subject number
+     const.PArandi = [1,2];
+ else
+     const.PArandi = [2,1];
+ end
+ 
+ 
+if mod(const.block,2) || const.DEBUG % if odd block number
     expDes.polarAngles = (possiblePAs(:,const.PArandi(1,1)))';
-
-else %eif const.block==2
-
-    npriorB = const.block-1;
-    completedPAs = [];
-    for bb=1:npriorB
-
-        % LOAD IN THE DSEIGN FILE FROM RUN 1, expDes.polarAngles
-        filename = sprintf('S%s_design_Block%i.mat',const.subjID,bb);
-        % % const.blockDir is your path strrep(const.blockDir, 'Block2', 'Block1')
-        block1dir = strrep(const.blockDir, sprintf('Block%s', num2str(const.block)), sprintf('Block%s', num2str(bb)));
-        tmp_oldsession = load(fullfile(block1dir, filename));
-        old_polarangles = tmp_oldsession.expDes.polarAngles;
-        
-        completedPAs = [completedPAs old_polarangles];
-
-    end
-
-    % TO DO: then select two randomly from possiblePAs that are NOT in this array that is in possible locs.
-    
-    drawfromPAs = setdiff(possiblePAs,completedPAs);
-     const.PArandi = randperm(length(drawfromPAs));
-
-    if length(drawfromPAs) <= 1
-        error('ONE OR LESS POLAR ANGLE LOCATIONS IN SESSION. PLEASE MODIFY.')
-    else
-        expDes.polarAngles = drawfromPAs(const.PArandi([1 2]));
-     end
+elseif ~mod(const.block,2) % if even block number
+    expDes.polarAngles = (possiblePAs(:,const.PArandi(1,2)))';
  
 end
 
